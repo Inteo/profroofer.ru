473ea6dcb3dd0b9cd4ade737a698fbe944842953
BerTepesh <Ber.Tepesh@gmail.com>
2015-10-14 13:35:12 +0300

    Начало

---
+++ f/js/common.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	$(".modal-inline").fancybox({
+    type: 'inline',
+    fixed:false,
+    title: '',
+    width: '100%',
+    height: 'auto',
+    padding: 0,
+    autoResize: false,
+    autoSize: true,
+    fitToView: false,
+    minWidth: 320,
+    scrolling   : 'no',
+    helpers: {
+      overlay: {
+        fixed: false
+      }
+    }
+  });
+});
+$(function(){
+	
+});
